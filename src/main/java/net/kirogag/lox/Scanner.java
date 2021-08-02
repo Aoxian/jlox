b8788ef7bb8679235b8036958fcae5f5dad4cbe6
Ian Gagorik <ian.gagorik@gmail.com>
2021-08-01 21:39:45 -0700

    Can scan single character tokens

--- src/main/java/net/kirogag/lox/Scanner.java
+++ src/main/java/net/kirogag/lox/Scanner.java
@@ -9,10 +9,33 @@ public class Scanner {
 
   Scanner(String source) {
     this.source = source;
-    tokens.add(new Token(TokenType.STRING, source, source, 1));
   }
 
   List<Token> scanTokens() {
+    scanToken(source);
     return tokens;
+  }
+
+  private void scanToken(String source) {
+    char c = source.charAt(0);
+    switch (c) {
+      case '(': addToken(TokenType.LEFT_PAREN, c); break;
+      case ')': addToken(TokenType.RIGHT_PAREN, c); break;
+      case '{': addToken(TokenType.LEFT_BRACE, c); break;
+      case '}': addToken(TokenType.RIGHT_BRACE, c); break;
+      case ',': addToken(TokenType.COMMA, c); break;
+      case '.': addToken(TokenType.DOT, c); break;
+      case '-': addToken(TokenType.MINUS, c); break;
+      case '+': addToken(TokenType.PLUS, c); break;
+      case ';': addToken(TokenType.SEMICOLON, c); break;
+      case '/': addToken(TokenType.SLASH, c); break;
+      case '*': addToken(TokenType.STAR, c); break;
+      default: addToken(TokenType.STRING, c); break;
+    }
+  }
+
+  private void addToken(TokenType type, char c) {
+    String text = Character.toString(c);
+    tokens.add(new Token(type, text, null, 1));
   }
 }
--- src/test/java/net/kirogag/lox/ScannerTest.java
+++ src/test/java/net/kirogag/lox/ScannerTest.java
@@ -2,15 +2,124 @@ package net.kirogag.lox;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScannerTest {
-  @Test
-  public void scanTokensTokenizesAStringAsOneToken() {
-    String loxStatement = "var language = \"lox\";";
-    Scanner testScanner = new Scanner(loxStatement);
-    Token firstToken = testScanner.scanTokens().get(0);
+  private Token scanFirstToken(String source) {
+    Scanner testScanner = new Scanner(source);
+    List<Token> tokens = testScanner.scanTokens();
+    return tokens.get(0);
+  }
 
-    assertEquals(loxStatement, firstToken.lexeme);
+  @Test
+  public void scanLeftParenToken() {
+    String leftParenToken = "(";
+    String expectedToken = TokenType.LEFT_PAREN + " " + leftParenToken;
+
+    Token firstToken = scanFirstToken(leftParenToken);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanRightParenToken() {
+    String rightParenToken = ")";
+    String expectedToken = TokenType.RIGHT_PAREN + " " + rightParenToken;
+
+    Token firstToken = scanFirstToken(rightParenToken);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanLeftBraceToken() {
+    String leftBraceToken = "{";
+    String expectedToken = TokenType.LEFT_BRACE + " " + leftBraceToken;
+
+    Token firstToken = scanFirstToken(leftBraceToken);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanRightBraceToken() {
+    String rightBraceToken = "}";
+    String expectedToken = TokenType.RIGHT_BRACE + " " + rightBraceToken;
+
+    Token firstToken = scanFirstToken(rightBraceToken);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanCommaToken() {
+    String comma = ",";
+    String expectedToken = TokenType.COMMA + " " + comma;
+
+    Token firstToken = scanFirstToken(comma);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanDotToken() {
+    String dot = ".";
+    String expectedToken = TokenType.DOT + " " + dot;
+
+    Token firstToken = scanFirstToken(dot);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanMinusToken() {
+    String minus = "-";
+    String expectedToken = TokenType.MINUS + " " + minus;
+
+    Token firstToken = scanFirstToken(minus);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanPlusToken() {
+    String plusToken = "+";
+    String expectedToken = TokenType.PLUS + " " + plusToken;
+
+    Token firstToken = scanFirstToken(plusToken);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanSemicolonToken() {
+    String semicolon = ";";
+    String expectedToken = TokenType.SEMICOLON + " " + semicolon;
+
+    Token firstToken = scanFirstToken(semicolon);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanSlashToken() {
+    String slash = "/";
+    String expectedToken = TokenType.SLASH + " " + slash;
+
+    Token firstToken = scanFirstToken(slash);
+
+    assertEquals(expectedToken, firstToken.toString());
+  }
+
+  @Test
+  public void scanStarToken() {
+    String star = "*";
+    String expectedToken = TokenType.STAR + " " + star;
+
+    Token firstToken = scanFirstToken(star);
+
+    assertEquals(expectedToken, firstToken.toString());
   }
 }
